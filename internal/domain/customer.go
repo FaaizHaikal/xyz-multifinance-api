c4e719456cd21ea3fde1d37c15ee87ca0e3258a7
FaaizHaikal <faaizhaikal@gmail.com>
2025-07-18 15:33:49 +0700

    feat: add password

--- internal/domain/customer.go
+++ internal/domain/customer.go
@@ -6,6 +6,7 @@ type Customer struct {
 	ID             string    `gorm:"primaryKey;type:char(36)" json:"id"`
 	NIK            string    `gorm:"unique;type:varchar(16)" json:"nik"`
 	FullName       string    `gorm:"type:varchar(100)" json:"full_name"`
+	Password       string    `gorm:"type:varchar(255)" json:"-"`
 	LegalName      string    `gorm:"type:varchar(100)" json:"legal_name"`
 	BirthPlace     string    `gorm:"type:varchar(100)" json:"birth_place"`
 	BirthDate      time.Time `gorm:"type:date" json:"birth_date"`
